24b2139d2386b754919dce3a54e38a5352c2248e
Ronaldo <RonaldoDe>
2020-04-26 23:20:27 -0500

    Menu Fixed

--- app/Http/Controllers/Api/Administration/MenuController.php
+++ app/Http/Controllers/Api/Administration/MenuController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Administration;
 
 use App\Http\Controllers\Controller;
+use App\Models\CUser;
+use App\Models\Master\BranchUser;
 use App\Models\Module;
 use App\Models\Permission;
 use App\User;
@@ -18,11 +20,30 @@ class MenuController extends Controller
     public function modulegeneral(Request $request)
     {
         # Get user
-        $user = User::on('connectionDB')->find(Auth::id());
+        $user = User::find(Auth::id());
+        $data = [
+            'phanton' => 0,
+            'master' => 0
+        ];
 
         # Validate if the user exists
         if(!$user){
             return response()->json(['response' => ['error' => ['Usuario no encontrado']]], 404);
+        }
+
+        $branch_user = BranchUser::where('user_id', $user->id)->first();
+        if(!$branch_user){
+            return response()->json(['response' => 'Free-user'], 200);
+        }
+        if($branch_user->branch_id == 1){
+            $user_id = $user->id;
+            $data['master'] = 1;
+        }else{
+            $c_user = CUser::on('connectionDB')->where('principal_id', $user->id)->first();
+            $user_id = $c_user->id;
+            if($c_user->phanton_user){
+                $data['phanton'] = 1;
+            }
         }
 
         # Get the modules with their respective permissions
@@ -33,6 +54,7 @@ class MenuController extends Controller
         ->join('user_has_role as ur', 'r.id', 'ur.role_id')
         ->join('users as u', 'ur.user_id', 'u.id')
         ->distinct('u.id')
+        ->where('u.id', $user_id)
         ->get();
 
         # Order by the module name
@@ -40,6 +62,6 @@ class MenuController extends Controller
         # Group by the module name
         $modules = collect($collection)->groupBy('module_name');
 
-        return response()->json(['response' => $modules], 200);
+        return response()->json(['response' => $modules, 'master' => $data['master'], 'phantom' => $data['phanton']], 200);
     }
 }

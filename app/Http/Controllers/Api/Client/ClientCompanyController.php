8e96cc25e97a25e42abed10cf1a3f3d2769f7a96
Ronaldo <RonaldoDe>
2020-05-03 23:05:23 -0500

    24 Hours

--- app/Http/Controllers/Api/Client/ClientCompanyController.php
+++ app/Http/Controllers/Api/Client/ClientCompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Helper\SetConnectionHelper;
 use App\Models\ClientTurn;
+use App\Models\CompanyData;
 use App\Models\Master\BranchOffice;
 use App\Models\Master\MasterCompany;
 use Illuminate\Http\Request;
@@ -65,15 +66,30 @@ class ClientCompanyController extends Controller
         #Set conection
         $set_new_connection = SetConnectionHelper::setByDBName($branch->db_name);
 
-        $turns_on_hold = ClientTurn::on($branch->db_name)->where('state_id', 2)->where('today', date('Y-m-d'))->count('turn_number');
-        $turns_in_process = ClientTurn::on($branch->db_name)->select('turn_number')->where('state_id', 1)->where('today', date('Y-m-d'))->get();
-        $current_turn = ClientTurn::on($branch->db_name)->select('turn_number')->where('state_id', 1)->where('today', date('Y-m-d'))->max('turn_number');
-        $last_turn = ClientTurn::on($branch->db_name)->select('turn_number')->whereIn('state_id', [1, 2])->where('today', date('Y-m-d'))->get();
+        #Branch data
+        $company_data = CompanyData::on($branch->db_name)->find(1);
 
-        $branch->turns_on_hold = $turns_on_hold;
-        $branch->turns_in_process = $turns_in_process;
-        $branch->current_turn = $current_turn;
-        $branch->turns_total = count($last_turn);
+        if(!$branch->hours_24){
+            $turns_on_hold = ClientTurn::on($branch->db_name)->where('state_id', 2)->where('today', date('Y-m-d'))->count('turn_number');
+            $turns_in_process = ClientTurn::on($branch->db_name)->select('turn_number')->where('state_id', 1)->where('today', date('Y-m-d'))->get();
+            $current_turn = ClientTurn::on($branch->db_name)->select('turn_number')->where('state_id', 1)->where('today', date('Y-m-d'))->max('turn_number');
+            $last_turn = ClientTurn::on($branch->db_name)->select('turn_number')->whereIn('state_id', [1, 2])->where('today', date('Y-m-d'))->get();
+
+            $branch->turns_on_hold = $turns_on_hold;
+            $branch->turns_in_process = $turns_in_process;
+            $branch->current_turn = $current_turn;
+            $branch->turns_total = count($last_turn);
+        }else{
+            $turns_on_hold = ClientTurn::on($branch->db_name)->where('state_id', 2)->where('c_return', $company_data->current_return)->count('turn_number');
+            $turns_in_process = ClientTurn::on($branch->db_name)->where('state_id', 1)->where('c_return', $company_data->current_return)->count('turn_number');
+            $current_turn = ClientTurn::on($branch->db_name)->select('turn_number')->where('state_id', 1)->where('c_return', $company_data->current_return)->max('turn_number');
+            $last_turn = ClientTurn::on($branch->db_name)->select('turn_number')->whereIn('state_id', [1, 2])->where('c_return', $company_data->current_return)->get();
+
+            $branch->turns_on_hold = $turns_on_hold;
+            $branch->turns_in_process = $turns_in_process;
+            $branch->current_turn = $current_turn;
+            $branch->turns_total = count($last_turn);
+        }
 
         return response()->json(['response' => $branch], 200);
     }

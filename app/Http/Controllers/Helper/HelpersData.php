6770535faac26b020f39637a5a7bd61eccdf4886
Ronaldo <RonaldoDe>
2020-06-27 18:40:37 -0500

    Employee list turn

--- app/Http/Controllers/Helper/HelpersData.php
+++ app/Http/Controllers/Helper/HelpersData.php
@@ -130,7 +130,7 @@ class HelpersData extends Controller
                     if($start == $key){
                         foreach ($value as $date) {
 
-                            dd([$date, $start_hout]);
+                            dd([$date, $start_hout, $date_start]);
 
                             if($start_hout >= $date->date_start && $start_hout <= $date->date_end){
                                 $pass_start += 1;

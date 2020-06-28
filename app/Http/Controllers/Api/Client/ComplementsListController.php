eb534470bab6a274516b592595c79e1d3f65de65
Ronaldo <RonaldoDe>
2020-06-27 19:53:52 -0500

    Employee list turn

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -157,7 +157,7 @@ class ComplementsListController extends Controller
                 }
 
                 if($pass > 0){
-                    $data_to_delete = collect($employees_valid)->search($client->employee_id);
+                    $data_to_delete = collect($employees_valid)->search($client->employee_id)->keys();
                     unset($employees_valid[$data_to_delete]);
                 }
                 return response()->json(['response' => [$employees_valid, $client_service, $pass, $client->employee_id]], 400);

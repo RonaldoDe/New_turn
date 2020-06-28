2f26a481b4bc8b476dd25506052b22b8009446e7
Ronaldo <RonaldoDe>
2020-06-27 19:24:38 -0500

    Employee list turn

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -125,7 +125,7 @@ class ComplementsListController extends Controller
 
             $collect = collect($employees)->pluck('id');
 
-            return response()->json(['response' => $employees], 400);
+            return response()->json(['response' => $validate_business_days], 400);
 
             $client_service = ClientService::on($branch->db_name)
             ->whereIn('employee_id', $collect)

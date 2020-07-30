c0f1d60ca826b84ab88d09f927b9a0f359588464
Ronaldo <RonaldoDe>
2020-07-29 20:56:37 -0500

    Test

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -250,7 +250,6 @@ class ComplementsListController extends Controller
             ->where('client_service.date_end', '<=', request('date').' 23:59:59')
             ->get();
 
-            return response()->json(['response' => $client_services], 200);
 
             foreach ($client_services as $client_master) {
                 $test = collect($employees)->pluck('id');
@@ -261,6 +260,7 @@ class ComplementsListController extends Controller
                 ->whereIn('employee_id', $test)
                 ->whereIn('state_id', [2, 5])
                 ->get();
+                return response()->json(['response' => $client_service], 200);
 
                 $pass = 0;
                 $employees_valid = $test;

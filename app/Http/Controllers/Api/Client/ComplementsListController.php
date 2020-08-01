218f84bc6be621c2f2eb7278e332ede4643f792a
Ronaldo <RonaldoDe>
2020-08-01 01:07:33 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -111,7 +111,7 @@ class ComplementsListController extends Controller
             }
 
 
-            $original_date_end = date('Y-m-d H:i:s', strtotime('+'.$service->time.' minute', strtotime(date(request('date_start')))));
+            $original_date_end = date('Y-m-d H:i:s', strtotime('+'.$service->unit_per_hour.' minute', strtotime(date(request('date_start')))));
             return response()->json(['response' => [$original_date_end]], 400);
 
             $validate_business_days = HelpersData::employeeBusinessDays(request('date_start'), $original_date_end, $service->id, $branch->db_name);

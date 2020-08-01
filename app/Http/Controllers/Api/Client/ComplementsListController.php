3e1cda66683a9e02bec2475f6cf9af9d5a43c69d
Ronaldo <RonaldoDe>
2020-08-01 00:34:30 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -273,9 +273,7 @@ class ComplementsListController extends Controller
                     $date_start = new DateTime($client_master->date_start);
                     $new_date_start = $date_start->modify('+'.$i.' minute')->format('Y-m-d H:i:s');
                     $new_date_end = date('Y-m-d H:i:s', strtotime('+'.$branch->minimun_time.' minute', strtotime($new_date_start)));
-                    if($client_master->id == 46){
-                        return response()->json(['response' => [$client_master, $i, $new_date_start]], 400);
-                    }
+
                     # Sumar los "10" min desde la hora inicial y hacer el resto de el código y validar esos tiempos por cada usuario
                     # Crear un registro por cada vuelta de el for, como si fuese un registro de db pero con las horas divididas por el minimo
 
@@ -343,6 +341,10 @@ class ComplementsListController extends Controller
                         "employee_last_name"=> $client_master->employee_last_name,
                         "available_employees"=> $employees_list
                     ));
+
+                    if($client_master->id == 46){
+                        return response()->json(['response' => [$client_master, $i, $new_date_start, $data_array]], 400);
+                    }
                 }
 
 

ef10995dc93c42ec4f8e5ab3b95c9cfa4bed14e9
Ronaldo <RonaldoDe>
2020-08-02 17:39:28 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -300,11 +300,8 @@ class ComplementsListController extends Controller
                         if($new_date_start > $client->date_start && $new_date_start < $client->date_end)
                         {
                             $pass++;
-                            if($client_master->id == 46){
-                                if($client->employee_id == 12){
-                                    return response()->json(['response' => 00], 400);
-                                }
-                                #return response()->json(['response' => 1, $new_date_end, $client->date_start, $client->date_end, $client->employee_id], 400);
+                            if($client_master->id == 46 && $client->employee_id == 12){
+                                return response()->json(['response' => 1, $new_date_end, $client->date_start, $client->date_end, $client_service], 400);
                             }
 
                         }
@@ -312,11 +309,8 @@ class ComplementsListController extends Controller
                         if($new_date_end > $client->date_start && $new_date_end < $client->date_end)
                         {
                             $pass++;
-                            if($client_master->id == 46){
-                                if($client->employee_id == 12){
-                                    return response()->json(['response' => 00], 400);
-                                }
-                                #return response()->json(['response' => 2, $new_date_end, $client->date_start, $client->date_end, $client->employee_id], 400);
+                            if($client_master->id == 46 && $client->employee_id == 12){
+                                return response()->json(['response' => 2, $new_date_end, $client->date_start, $client->date_end, $client_service], 400);
                             }
 
                         }
@@ -324,11 +318,8 @@ class ComplementsListController extends Controller
                         if($client->date_start > $new_date_start && $client->date_start < $new_date_end)
                         {
                             $pass++;
-                            if($client_master->id == 46){
-                                if($client->employee_id == 12){
-                                    return response()->json(['response' => 00], 400);
-                                }
-                                #return response()->json(['response' => 3, $new_date_end, $client->date_start, $client->date_end, $client->employee_id], 400);
+                            if($client_master->id == 46 && $client->employee_id == 12){
+                                return response()->json(['response' => 3, $new_date_end, $client->date_start, $client->date_end, $client_service], 400);
                             }
 
                         }
@@ -337,10 +328,7 @@ class ComplementsListController extends Controller
                         {
                             $pass++;
                             if($client_master->id == 46 && $client->employee_id == 12){
-                                if($client->employee_id == 12){
-                                    return response()->json(['response' => 00], 400);
-                                }
-                                #return response()->json(['response' => 4, $new_date_end, $client->date_start, $client->date_end, $client->employee_id], 400);
+                                return response()->json(['response' => 4, $new_date_end, $client->date_start, $client->date_end, $client_service], 400);
                             }
 
                         }

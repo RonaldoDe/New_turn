ff37979ca2f0a5afd7b2f2cb19834be4a1ec6a04
Ronaldo <RonaldoDe>
2020-08-02 01:20:54 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -315,7 +315,7 @@ class ComplementsListController extends Controller
                         }
 
                         if($client_master->id == 46 && $pass > 0 && $client->employee_id == 12){
-                            return response()->json(['response' => $employees_valid, $pass], 400);
+                            return response()->json(['response' => $employees_valid, $pass, $client], 400);
                         }
                         if($pass > 0){
                             $data_to_delete = collect($employees_valid)->search($client->employee_id);

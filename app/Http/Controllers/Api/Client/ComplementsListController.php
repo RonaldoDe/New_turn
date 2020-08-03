dd9f4e8a4208667b788b3629ae8e9f862f1333a5
Ronaldo <RonaldoDe>
2020-08-02 20:37:22 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -326,8 +326,8 @@ class ComplementsListController extends Controller
                             unset($employees_valid[$data_to_delete]);
                         }
 
-                        if($client_master->id == 44){
-                            return response()->json(['response' => [$client, $employees_valid, 'new' => [$new_date_start, $new_date_end]]], 400);
+                        if($client_master->id == 44 && $client->id == 13){
+                            return response()->json(['response' => [$client, $employees_valid, 'new' => [$new_date_start, $new_date_end], $pass]], 400);
                         }
 
                     }

3a23ded962f3ac743eed1635fe4fcc35397e7a02
Ronaldo <RonaldoDe>
2020-08-02 20:41:14 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -300,24 +300,35 @@ class ComplementsListController extends Controller
                         if($new_date_start > $client->date_start && $new_date_start <= $client->date_end)
                         {
                             $pass++;
-
+                            if($client_master->id == 44 && $client->employee_id == 13){
+                                return response()->json(['response' => [1]], 400);
+                            }
                         }
 
                         if($new_date_end >= $client->date_start && $new_date_end < $client->date_end)
                         {
                             $pass++;
+                            if($client_master->id == 44 && $client->employee_id == 13){
+                                return response()->json(['response' => [2]], 400);
+                            }
 
                         }
 
                         if($client->date_start >= $new_date_start && $client->date_start <= $new_date_end)
                         {
                             $pass++;
+                            if($client_master->id == 44 && $client->employee_id == 13){
+                                return response()->json(['response' => [3]], 400);
+                            }
 
                         }
 
                         if($client->date_end > $new_date_start && $client->date_end <= $new_date_end)
                         {
                             $pass++;
+                            if($client_master->id == 44 && $client->employee_id == 13){
+                                return response()->json(['response' => [4]], 400);
+                            }
 
                         }
 

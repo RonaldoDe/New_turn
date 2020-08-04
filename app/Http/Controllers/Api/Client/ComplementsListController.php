f8072e7a8c81fc2487c22fbde80a2a06154c673f
Ronaldo <RonaldoDe>
2020-08-04 02:15:17 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -158,14 +158,14 @@ class ComplementsListController extends Controller
                     # Validar los rangos de fechas
                     if($new_date_start > $client->date_start && $new_date_start < $client->date_end)
                     {
-                        if($branch->minimun_time <= $service->unit_per_hour){
+                        if($branch->minimun_time < $service->unit_per_hour){
                             $pass++;
                         }
                     }
 
                     if($new_date_end > $client->date_start && $new_date_end <= $client->date_end)
                     {
-                        if($branch->minimun_time <= $service->unit_per_hour){
+                        if($branch->minimun_time < $service->unit_per_hour){
                             $pass++;
                         }
 
@@ -173,7 +173,7 @@ class ComplementsListController extends Controller
 
                     if($client->date_start >= $new_date_start && $client->date_start < $new_date_end)
                     {
-                        if($branch->minimun_time <= $service->unit_per_hour){
+                        if($branch->minimun_time < $service->unit_per_hour){
                             $pass++;
                         }
 
@@ -181,7 +181,7 @@ class ComplementsListController extends Controller
 
                     if($client->date_end > $new_date_start && $client->date_end <= $new_date_end)
                     {
-                        if($branch->minimun_time <= $service->unit_per_hour){
+                        if($branch->minimun_time < $service->unit_per_hour){
                             $pass++;
                         }
                     }
@@ -315,11 +315,13 @@ class ComplementsListController extends Controller
                     $employees_valid = $test;
                     foreach ($client_service as $client) {
                         $pass = 0;
-
+                                /*if($client->employee_id == 13 && $client_master->id == 50 && $i == 0){
+                                    return response()->json(['response' => $client, $new_date_start, $new_date_end, $pass, 'minimun and max' => [$branch->minimun_time, $service->unit_per_hour]], 400);
+                                }*/
                         # Validar los rangos de fechas
                         if($new_date_start > $client->date_start && $new_date_start < $client->date_end)
                         {
-                            if($branch->minimun_time <= $service->unit_per_hour){
+                            if($branch->minimun_time < $service->unit_per_hour){
                                $pass++;
                             }
                         }
@@ -328,9 +330,6 @@ class ComplementsListController extends Controller
                         {
 
                             if($branch->minimun_time < $service->unit_per_hour){
-                                /*if($client->employee_id == 13 && $client_master->id == 50 && $i == 0){
-                                    return response()->json(['response' => $client, $new_date_start, $new_date_end, $pass, 'minimun and max' => [$branch->minimun_time, $service->unit_per_hour]], 400);
-                                }*/
                                 $pass++;
                             }
 
@@ -338,7 +337,7 @@ class ComplementsListController extends Controller
 
                         if($client->date_start >= $new_date_start && $client->date_start < $new_date_end)
                         {
-                            if($branch->minimun_time <= $service->unit_per_hour){
+                            if($branch->minimun_time < $service->unit_per_hour){
                                 $pass++;
                             }
 
@@ -346,7 +345,7 @@ class ComplementsListController extends Controller
 
                         if($client->date_end > $new_date_start && $client->date_end <= $new_date_end)
                         {
-                            if($branch->minimun_time <= $service->unit_per_hour){
+                            if($branch->minimun_time < $service->unit_per_hour){
                                 $pass++;
                             }
 

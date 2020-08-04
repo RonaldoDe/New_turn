e399af0ec719233531d6c785d0a1a3532975c399
Ronaldo <RonaldoDe>
2020-08-04 01:44:47 -0500

    Fix

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -330,7 +330,7 @@ class ComplementsListController extends Controller
 
                             if($branch->minimun_time <= $service->unit_per_hour){
                                 if($client->employee_id == 10 && $client->id == 50){
-                                    return response()->json(['response' => $client, $new_date_start, $new_date_end, $pass, 'minimun and max' => [$branch->minimun_time, $service->unit_per_hour]], 400);
+                                    return response()->json(['response' => $client_master, $new_date_start, $new_date_end, $pass, 'minimun and max' => [$branch->minimun_time, $service->unit_per_hour]], 400);
                                 }
                                 $pass++;
                             }

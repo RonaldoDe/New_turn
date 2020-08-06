b0e33f6991e86c77ab48b5294452bb68d95dfb90
Ronaldo <RonaldoDe>
2020-08-05 21:14:05 -0500

    Limit hours

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -352,7 +352,9 @@ class ComplementsListController extends Controller
 
                         }
 
-                        return response()->json(['response' => [$client, $new_date_start, $new_date_end]], 400);
+                        if($client_master->id == 59){
+                            return response()->json(['response' => [$client, $new_date_start, $new_date_end]], 400);
+                        }
                         if($new_date_start >= $client->date_start && $service->unit_per_hour > $branch->minimun_time){
                         }
 

d0c4dfd1238b1439ab560a0fdb4742090eb8d189
Ronaldo <RonaldoDe>
2020-08-05 20:06:44 -0500

    Limit hours

--- app/Http/Controllers/Api/Client/ComplementsListController.php
+++ app/Http/Controllers/Api/Client/ComplementsListController.php
@@ -322,7 +322,7 @@ class ComplementsListController extends Controller
                         # Validar los rangos de fechas
                         if($new_date_start > $client->date_start && $new_date_start < $client->date_end)
                         {
-                            if($branch->minimun_time < $service->unit_per_hour){
+                            if($branch->minimun_time <= $service->unit_per_hour){
                                $pass++;
                             }
                         }
@@ -330,7 +330,7 @@ class ComplementsListController extends Controller
                         if($new_date_end >= $client->date_start && $new_date_end <= $client->date_end)
                         {
 
-                            if($branch->minimun_time <= $service->unit_per_hour){
+                            if($branch->minimun_time < $service->unit_per_hour){
                                 $pass++;
                             }
 

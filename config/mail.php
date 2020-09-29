6a69edaa152387d8442d82d68a83d129ab04a012
ronaldo camacho meza <ronaldocamachomeza@hotmail.com>
2020-09-28 22:33:40 -0500

    Email

--- config/mail.php
+++ config/mail.php
@@ -120,14 +120,6 @@ return [
         ],
     ],
 
-    'stream' => [
-        'ssl' => [
-           'allow_self_signed' => true,
-           'verify_peer' => false,
-           'verify_peer_name' => false,
-        ],
-     ],
-
     /*
     |--------------------------------------------------------------------------
     | Log Channel
